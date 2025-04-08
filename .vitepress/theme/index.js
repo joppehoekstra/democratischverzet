d5dfe1cbb750123feea25f5a33bb5ed9d1f23b33
Joppe Hoekstra <joppehoekstra@icloud.com>
2025-04-08 14:05:23 +0200

    Add tracking

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
 import './styles.css'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_hJNoZFxoTQWKuyXv1BY8F7wdXSnNG82RETWiacs9jlV',
+  {
+    ui_host: 'https://eu.posthog.com',
+    api_host: 'https:/a.democratischverzet.nl',
+    person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+  }
+)
+
+
 export default DefaultTheme

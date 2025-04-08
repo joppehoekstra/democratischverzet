37e1231925e605fa77a1c349ec286bf9a058c983
Joppe Hoekstra <joppehoekstra@icloud.com>
2025-04-08 14:22:31 +0200

    Fix typo

--- .vitepress/theme/MyLayout.vue
+++ .vitepress/theme/MyLayout.vue
@@ -9,7 +9,7 @@ onMounted(() => {
   posthog.init('phc_hJNoZFxoTQWKuyXv1BY8F7wdXSnNG82RETWiacs9jlV',
     {
       ui_host: 'https://eu.posthog.com',
-      api_host: 'https:/a.democratischverzet.nl',
+      api_host: 'https://a.democratischverzet.nl',
       person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
     }
   )
